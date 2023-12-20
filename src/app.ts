ee92ae65e44e441d6d64f683dc5c895a03ab9ec0
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-12-20 17:48:05 +0100

    refactor: move swagger configuration to another file

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,19 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import routes from './routes/routes';
+import { generateSwaggerDocs, serveSwaggerUi, setupSwaggerUi } from './utils/swaggerConfig';
 
 dotenv.config();
 
 const app: Express = express();
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 
-// Swagger setup
-const swaggerOptions = {
-    definition: {
-      openapi: '3.0.3',
-      info: {
-        title: 'Ascent Essentials - Backend API',
-        version: '0.1.0',
-        description: 'Documentation for Ascent Essentials backend',
-        servers: [`http://localhost:${process.env.PORT || 3000}`],
-      },
-    },
-    // Path to the API docs
-    apis: ['./routes/*.ts'],
-};
+// Auto-generated Swagger docs
+const swaggerDocs = generateSwaggerDocs();
 
-// auto generated swagger docs
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// Serve and setup Swagger UI
+app.use('/api-docs', serveSwaggerUi(), setupSwaggerUi(swaggerDocs));
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
-});
+// Definition of the routes
+app.use('/', routes);
 
 export default app;
---
+++ src/utils/swaggerConfig.ts
@@ -0,0 +1,29 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+export const generateSwaggerDocs = () => {
+    const swaggerOptions = {
+        definition: {
+            openapi: '3.0.3',
+            info: {
+                title: 'Ascent Essentials - Backend API',
+                version: '0.1.0',
+                description: 'Documentation for Ascent Essentials backend',
+                servers: [`http://localhost:${process.env.PORT || 3000}`],
+            },
+        },
+        // Path to the API docs
+        apis: ['./src/routes/*.ts'],
+
+    };
+
+    return swaggerJsdoc(swaggerOptions);
+};
+
+export const serveSwaggerUi = () => {
+    return swaggerUi.serve;
+};
+
+export const setupSwaggerUi = (swaggerDocs: any) => {
+    return swaggerUi.setup(swaggerDocs);
+};
