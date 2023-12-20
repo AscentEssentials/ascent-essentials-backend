1442fe5be5bd9c299669bb746231e9afeb238376
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-12-20 19:37:16 +0100

    feat: add Category schema for swagger documentation

---
+++ src/models/categoryModel.ts
@@ -0,0 +1,24 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Category:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the category
+ *         name:
+ *           type: string
+ *           description: The name of the category
+ *         description:
+ *           type: string
+ *           description: The description of the category
+ *       example:
+ *         id: "1"
+ *         name: "Category 1"
+ *         description: "This is the description of Category 1"
+ */
