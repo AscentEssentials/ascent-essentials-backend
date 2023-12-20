e94a868c9f8a44aaa1157b2aa7a67f3b85e52d3e
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-12-20 15:57:46 +0100

    feat: provide routes to consult API

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,33 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
+// Swagger setup
+const swaggerOptions = {
+    definition: {
+      openapi: '3.0.3',
+      info: {
+        title: 'Ascent Essentials - Backend API',
+        version: '0.1.0',
+        description: 'Documentation for Ascent Essentials backend',
+        servers: [`http://localhost:${process.env.PORT || 3000}`],
+      },
+    },
+    // Path to the API docs
+    // apis: ['./routes/*.js'],
+    apis: ['app.js']
+};
+
+// auto generated swagger docs
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// manually defined api, for our reference while developing
+const manualApi = YAML.load('./docs/openapi.yaml');
+app.use('/api-docs-manual', swaggerUi.serve, swaggerUi.setup(manualApi));
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
