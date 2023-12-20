faecc96ef3caebeba0846d2eec8e2c7eb17fd05b
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-12-20 19:37:58 +0100

    feat: add file to watch to generate api from

--- src/utils/swaggerConfig.ts
+++ src/utils/swaggerConfig.ts
@@ -1,29 +1,28 @@
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 export const generateSwaggerDocs = () => {
-    const swaggerOptions = {
-        definition: {
-            openapi: '3.0.3',
-            info: {
-                title: 'Ascent Essentials - Backend API',
-                version: '0.1.0',
-                description: 'Documentation for Ascent Essentials backend',
-                servers: [`http://localhost:${process.env.PORT || 3000}`],
-            },
-        },
-        // Path to the API docs
-        apis: ['./src/routes/*.ts'],
+  const swaggerOptions = {
+    definition: {
+      openapi: "3.0.3",
+      info: {
+        title: "Ascent Essentials - Backend API",
+        version: "0.1.0",
+        description: "Documentation for Ascent Essentials backend",
+        servers: [`http://localhost:${process.env.PORT || 3000}`],
+      },
+    },
+    // Path to the API docs
+    apis: ["./src/routes/*.ts", "./src/models/*.ts"],
+  };
 
-    };
-
-    return swaggerJsdoc(swaggerOptions);
+  return swaggerJsdoc(swaggerOptions);
 };
 
 export const serveSwaggerUi = () => {
-    return swaggerUi.serve;
+  return swaggerUi.serve;
 };
 
 export const setupSwaggerUi = (swaggerDocs: any) => {
-    return swaggerUi.setup(swaggerDocs);
+  return swaggerUi.setup(swaggerDocs);
 };
