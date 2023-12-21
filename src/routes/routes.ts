20e00040cd879635e273cbf0980c8c1adf498082
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-12-21 11:25:21 +0100

    docs: refer to the Product schema in the swagger routes documentation

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 /**
  * Express router for handling product-related routes.
@@ -21,7 +21,7 @@ const router = express.Router();
  */
 
 router.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+  res.send("Express + TypeScript Server");
 });
 
 /**
@@ -33,15 +33,23 @@ router.get("/", (req: Request, res: Response) => {
  *     responses:
  *       200:
  *         description: Returns all products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ *       400:
+ *         description: Bad request
  *       404:
  *         description: No products found
  *       500:
  *         description: Internal server error
  */
 
-router.get('/products', (req: Request, res: Response) => {
-    // Logic to fetch all products from the database
-    res.send('Get all products');
+router.get("/products", (req: Request, res: Response) => {
+  // Logic to fetch all products from the database
+  res.send("Get all products");
 });
 
 /**
@@ -50,14 +58,30 @@ router.get('/products', (req: Request, res: Response) => {
  *   post:
  *     tags: [Products]
  *     summary: Create a new product
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
  *     responses:
- *       200:
- *         description: Returns a success message
+ *       '201':
+ *         description: Product created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       '400':
+ *         description: Bad request
+ *       '401':
+ *         description: Unauthorized
+ *       '500':
+ *         description: Internal server error
  */
 
-router.post('/products', (req: Request, res: Response) => {
-    // Logic to create a new product in the database
-    res.send('Create a new product');
+router.post("/products", (req: Request, res: Response) => {
+  // Logic to create a new product in the database
+  res.send("Create a new product");
 });
 
 /**
@@ -73,17 +97,25 @@ router.post('/products', (req: Request, res: Response) => {
  *         schema:
  *           type: string
  *     responses:
- *       200:
- *         description: Returns the product with the specified ID
+ *       '200':
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       '400':
+ *         description: Bad request
+ *       '404':
+ *         description: Not found
+ *       '500':
+ *         description: Internal server error
  */
 
-router.get('/products/:id', (req: Request, res: Response) => {
-    const productId = req.params.id;
-    // Logic to fetch a specific product by ID from the database
-    res.send(`Get product with ID ${productId}`);
+router.get("/products/:id", (req: Request, res: Response) => {
+  const productId = req.params.id;
+  // Logic to fetch a specific product by ID from the database
+  res.send(`Get product with ID ${productId}`);
 });
-
-
 
 /**
  * @swagger
@@ -97,15 +129,31 @@ router.get('/products/:id', (req: Request, res: Response) => {
  *         required: true
  *         schema:
  *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
  *     responses:
- *       200:
- *         description: Returns a success message
+ *       '200':
+ *         description: Product updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       '400':
+ *         description: Bad request
+ *       '404':
+ *         description: Not found
+ *       '500':
+ *         description: Internal server error
  */
 
-router.put('/products/:id', (req: Request, res: Response) => {
-    const productId = req.params.id;
-    // Logic to update a specific product by ID in the database
-    res.send(`Update product with ID ${productId}`);
+router.put("/products/:id", (req: Request, res: Response) => {
+  const productId = req.params.id;
+  // Logic to update a specific product by ID in the database
+  res.send(`Update product with ID ${productId}`);
 });
 
 /**
@@ -121,14 +169,20 @@ router.put('/products/:id', (req: Request, res: Response) => {
  *         schema:
  *           type: string
  *     responses:
- *       200:
- *         description: Returns a success message
+ *       '204':
+ *         description: Product deleted successfully
+ *       '400':
+ *         description: Bad request
+ *       '404':
+ *         description: Not found
+ *       '500':
+ *         description: Internal server error
  */
 
-router.delete('/products/:id', (req: Request, res: Response) => {
-    const productId = req.params.id;
-    // Logic to delete a specific product by ID from the database
-    res.send(`Delete product with ID ${productId}`);
+router.delete("/products/:id", (req: Request, res: Response) => {
+  const productId = req.params.id;
+  // Logic to delete a specific product by ID from the database
+  res.send(`Delete product with ID ${productId}`);
 });
 
 export default router;
