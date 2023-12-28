fdd6907097421be16c8c47797277114909d480f9
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-12-29 00:29:36 +0100

    docs: update openapi Category schema, removing id

--- src/models/categoryModel.ts
+++ src/models/categoryModel.ts
@@ -7,12 +7,8 @@ import mongoose, { Model, Schema } from 'mongoose';
  *     Category:
  *       type: object
  *       required:
- *         - id
  *         - name
  *       properties:
- *         id:
- *           type: string
- *           description: The auto-generated id of the category
  *         name:
  *           type: string
  *           description: The name of the category
@@ -20,7 +16,6 @@ import mongoose, { Model, Schema } from 'mongoose';
  *           type: string
  *           description: The description of the category
  *       example:
- *         id: "1"
  *         name: "Category 1"
  *         description: "This is the description of Category 1"
  */
