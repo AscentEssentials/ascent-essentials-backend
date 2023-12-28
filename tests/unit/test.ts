6d4234f7ca9cbe572ece2ecac21d4fa9e34bed0f
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-12-29 00:47:21 +0100

    test: fix test

--- tests/unit/test.ts
+++ tests/unit/test.ts
@@ -1,9 +1,15 @@
 import request from "supertest";
 import app from "../../src/app";
+import mongoose from "mongoose";
 
 describe("Test the root path", () => {
-    test("It should respond to the GET method", async () => {
-        const response = await request(app).get("/");
-        expect(response.statusCode).toBe(200);
-    });
+  test("It should respond to the GET method", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
+  });
+
+  afterAll(async () => {
+    // Close the MongoDB connection after all tests are done
+    await mongoose.connection.close();
+  });
 });
