b90d97e84d9d3b8bd663cff610b125fcae4a07a6
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-12-29 15:55:21 +0100

    test: change status code to check

--- tests/unit/test.ts
+++ tests/unit/test.ts
@@ -2,10 +2,10 @@ import request from "supertest";
 import app from "../../src/app";
 import mongoose from "mongoose";
 
-describe("Test the root path", () => {
+describe("Test the health path", () => {
   test("It should respond to the GET method", async () => {
-    const response = await request(app).get("/");
-    expect(response.statusCode).toBe(200);
+    const response = await request(app).get("/health");
+    expect(response.statusCode).toBe(503);
   });
 
   afterAll(async () => {
