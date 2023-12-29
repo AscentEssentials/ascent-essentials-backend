d1111e16639ddec1365be442880b12c62a333a6e
Eddie Barzi <eddie.barzi@studio.unibo.it>
2023-12-29 17:00:46 +0100

    feat: add Product model

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -1,3 +1,5 @@
+import mongoose, { Model, Schema, Types } from "mongoose";
+
 /**
  * @swagger
  * components:
@@ -5,39 +7,104 @@
  *     Product:
  *       type: object
  *       required:
- *         - id
  *         - name
- *         - category
+ *         - brand
  *         - price
+ *         - category
  *         - description
- *         - availableQuantity
+ *         - quantity
  *       properties:
- *         id:
- *           type: string
- *           description: The auto-generated id of the product
  *         name:
  *           type: string
  *           description: The name of the product
- *         category:
- *           $ref: '#/components/schemas/Category'
- *           description: The category of the product
+ *         brand:
+ *           type: string
+ *           description: The brand of the product
  *         price:
  *           type: number
- *           format: float
  *           description: The price of the product
+ *         category:
+ *           type: string
+ *           description: The ID of the category to which the product belongs
  *         description:
  *           type: string
  *           description: The description of the product
- *         availableQuantity:
+ *         technicalSpecifications:
+ *           type: object
+ *           description: JSON field for technical specifications
+ *         quantity:
  *           type: number
+ *           minimum: 0
  *           description: The available quantity of the product
  *       example:
- *         id: "1"
  *         name: "Product 1"
- *         category:
- *           id: "1"
- *           name: "Category 1"
- *         price: 9.99
- *         description: "This is a product"
- *         availableQuantity: 10
+ *         brand: "Brand A"
+ *         price: 19.99
+ *         category: "60a9b8e5b6f9b7287418b16c"
+ *         description: "This is the description of Product 1"
+ *         technicalSpecifications: { color: "red", stars: 2 }
+ *         quantity: 50
  */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     ProductResponse:
+ *       allOf:
+ *         - $ref: '#/components/schemas/Product'
+ *         - type: object
+ *           properties:
+ *             _id:
+ *               type: string
+ *               description: The auto-generated ID of the product
+ *           example:
+ *             _id: "60a9b9a3b6f9b7287418b16d"
+ *             name: "Product 1"
+ *             brand: "Brand A"
+ *             price: 19.99
+ *             category: "60a9b8e5b6f9b7287418b16c"
+ *             description: "This is the description of Product 1"
+ *             technicalSpecifications: { color: "red", stars: 2 }
+ *             quantity: 50
+ */
+
+/**
+ * Schema of a product for MongoDB
+ */
+export interface IProductDocument extends mongoose.Document {
+  name: string;
+  brand: string;
+  price: mongoose.Types.Decimal128;
+  category: mongoose.Schema.Types.ObjectId;
+  description: string;
+  technicalSpecifications: Record<string, unknown>; // JSON field
+  quantity: number;
+}
+
+const productSchema = new Schema<IProductDocument>({
+  name: { type: String, required: true },
+  brand: { type: String, required: true },
+  price: { type: Types.Decimal128, required: true },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+    validate: {
+      validator: async function (value: mongoose.Types.ObjectId) {
+        const category = await mongoose.model("Category").findById(value);
+        return !!category;
+      },
+      message: "Invalid category reference",
+    },
+  },
+  description: { type: String, required: true },
+  technicalSpecifications: { type: Schema.Types.Mixed }, // JSON field
+  quantity: { type: Number, required: true, min: 0 },
+});
+
+const productModel: Model<IProductDocument> = mongoose.model(
+  "Product",
+  productSchema
+);
+export default productModel;
