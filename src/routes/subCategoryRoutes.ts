38862efe78eb68c9b85b39e5ee588c5a0f6df1cd
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-03 12:31:54 +0100

    feat! add route to Get details of a subcategory

--- src/routes/subCategoryRoutes.ts
+++ src/routes/subCategoryRoutes.ts
@@ -26,6 +26,35 @@ router.get("/subcategories", SubCategoryController.getAllSubCategories);
 /**
  * @swagger
  * /subcategory:
+ *   get:
+ *     tags: [Categories]
+ *     summary: Get details of a subcategory
+ *     parameters:
+ *       - in: query
+ *         name: id
+ *         required: true
+ *         description: The ID of the subcategory to retrieve
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns details of a subcategory
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SubCategoryResponse'
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: Category not found
+ *       500:
+ *         description: Internal server error
+ */
+router.get("/subcategory", SubCategoryController.getSubCategory);
+
+/**
+ * @swagger
+ * /subcategory:
  *   post:
  *     tags: [Categories]
  *     summary: Create a new subcategory
