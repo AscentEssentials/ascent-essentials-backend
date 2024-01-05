87840812ee26d18df06cdcb85f08ab758982534b
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-05 16:51:17 +0100

    feat!: user routes to register and login

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,81 @@
+import express from "express";
+import UserController from "../controllers/userController";
+
+const router = express.Router();
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: API endpoints for user registration and login
+ */
+
+/**
+ * @swagger
+ *   /register:
+ *     post:
+ *       summary: Register a new user
+ *       tags: [Users]
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       responses:
+ *         201:
+ *           description: User registered successfully
+ *         400:
+ *           description: Bad request or missing required fields
+ *         500:
+ *           description: Internal server error
+ */
+router.post("/register", UserController.registerUser);
+
+/**
+ * @swagger
+ *   /login:
+ *     post:
+ *       summary: Login as a user
+ *       tags: [Users]
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               required:
+ *                 - email
+ *                 - password
+ *               properties:
+ *                 email:
+ *                   type: string
+ *                   format: email
+ *                   description: The email address of the user
+ *                 password:
+ *                   type: string
+ *                   format: password
+ *                   description: The password of the user
+ *             example:
+ *               email: "john.doe@example.com"
+ *               password: "securepassword"
+ *       responses:
+ *         200:
+ *           description: User logged in successfully, returns JWT token
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   token:
+ *                     type: string
+ *                     description: JWT token for authentication
+ *         400:
+ *           description: Bad request or missing required fields
+ *         401:
+ *           description: Invalid email or password
+ *         500:
+ *           description: Internal server error
+ */
+router.post("/login", UserController.loginUser);
+
+export default router;
