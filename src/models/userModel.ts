ac7d01b48a682c12afdfe8975d5274026b0ec821
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-05 16:51:29 +0100

    docs: add swagger doc for user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,5 +1,48 @@
 import mongoose, { Document, Model, Schema } from "mongoose";
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - surname
+ *         - email
+ *         - address
+ *         - telephoneNumber
+ *         - password
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the user
+ *         surname:
+ *           type: string
+ *           description: The surname of the user
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: The email address of the user
+ *         address:
+ *           type: string
+ *           description: The address of the user
+ *         telephoneNumber:
+ *           type: string
+ *           description: The telephone number of the user
+ *         password:
+ *           type: string
+ *           format: password
+ *           description: The password of the user
+ *       example:
+ *         name: "John"
+ *         surname: "Doe"
+ *         email: "john.doe@example.com"
+ *         address: "123 Main Street, City"
+ *         telephoneNumber: "+1234567890"
+ *         password: "securepassword"
+ */
+
 export interface IUserDocument extends Document {
   name: string;
   surname: string;
