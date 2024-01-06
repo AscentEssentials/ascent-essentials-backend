971ead9095d6fdfe91431b5f8da31ac5b28a4c2e
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-06 12:03:32 +0100

    feat: improve authentication.ts
    - Created a custom AuthenticatedRequest interface to extend the standard Request type
    - Added user details to the AuthenticatedRequest interface for improved type safety
    - Introduced isAdmin middleware to check and handle admin access
    - Updated error handling in the authenticateToken middleware for more informative responses

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,17 +1,50 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import UserModel, { IUserDocument } from "../models/userModel";
 
-export const jwt_secret = process.env.JWT_SECRET || "a-really-(in)secure-secret-key";
+export const jwt_secret =
+  process.env.JWT_SECRET || "a-really-(in)secure-secret-key";
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+interface AuthenticatedRequest extends Request {
+  user?: IUserDocument;
+}
+
+export const authenticateToken = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
   const token = req.header("Authorization")?.split(" ")[1];
 
   if (!token) return res.status(401).send("Unauthorized");
 
-  jwt.verify(token, jwt_secret, (err, user) => {
-    if (err) return res.status(403).send("Forbidden");
+  try {
+    const decoded: any = jwt.verify(token, jwt_secret);
+    const user = await UserModel.findById(decoded.userId);
 
+    if (!user) {
+      return res.status(401).send("Invalid user.");
+    }
+
+    req.user = user; // Store user details on req.user
     next();
-  });
+  } catch (error) {
+    console.error("[AuthMiddleware] Error:", error);
+    res.status(401).send("Invalid token.");
+  }
 };
 
+export const isAdmin = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = req.user as IUserDocument;
+
+  if (!user || !user.isAdmin) {
+    console.error("[AuthMiddleware] Access denied. User is not an admin.");
+    return res.status(403).send("Access denied. User is not an admin.");
+  }
+
+  next();
+};
