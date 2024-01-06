e785b21b7f1aaec6e76860b74ef725a62c197dea
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-06 10:14:33 +0100

    feat: add auth middleware

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,2 +1,17 @@
-export const jwt_secret =
-  process.env.JWT_SECRET || "a-really-secure-secret-key";
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export const jwt_secret = process.env.JWT_SECRET || "a-really-(in)secure-secret-key";
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+
+  if (!token) return res.status(401).send("Unauthorized");
+
+  jwt.verify(token, jwt_secret, (err, user) => {
+    if (err) return res.status(403).send("Forbidden");
+
+    next();
+  });
+};
+
