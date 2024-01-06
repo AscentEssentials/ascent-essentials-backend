5edb3d11551e6ac16d8109c4212d2f0d923cda1b
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-06 14:47:22 +0100

    docs: add UserResponse schema for swagger

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -53,6 +53,45 @@ import mongoose, { Document, Model, Schema } from "mongoose";
  *         password: "securepassword"
  */
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     UserResponse:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the user
+ *         surname:
+ *           type: string
+ *           description: The surname of the user
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: The email address of the user
+ *         address:
+ *           type: string
+ *           description: The address of the user
+ *         addressNumber:
+ *           type: string
+ *           description: The address number of the user
+ *         zipCode:
+ *           type: string
+ *           description: The zip code of the user's address
+ *         telephoneNumber:
+ *           type: string
+ *           description: The telephone number of the user
+ *       example:
+ *         name: "John"
+ *         surname: "Doe"
+ *         email: "john.doe@example.com"
+ *         address: "123 Main Street, City"
+ *         addressNumber: "456"
+ *         zipCode: "12345"
+ *         telephoneNumber: "+1234567890"
+ */
+
 export interface IUserDocument extends Document {
   name: string;
   surname: string;
@@ -69,7 +108,7 @@ const userSchema = new Schema<IUserDocument>(
   {
     name: { type: String, required: true },
     surname: { type: String, required: true },
-    email: { type: String, required: true, unique: true }, // Using unique: true implies the creation of an index
+    email: { type: String, required: true, unique: true }, // Using "unique: true" implies the creation of an index
     address: { type: String, required: true },
     addressNumber: { type: String, required: true },
     zipCode: { type: String, required: true },
