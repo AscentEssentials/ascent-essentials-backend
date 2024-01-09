02c26ecf98ce3f0c1488ea5d5bc5aa204f4e53c7
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-09 11:35:23 +0100

    docs: add _id in swagger doc for Order schema

--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -1,5 +1,5 @@
-import mongoose, {Model, Schema} from "mongoose";
-import {IProductDocument} from "./productModel";
+import mongoose, { Model, Schema } from "mongoose";
+import { IProductDocument } from "./productModel";
 
 /**
  * @swagger
@@ -8,6 +8,9 @@ import {IProductDocument} from "./productModel";
  *     Order:
  *       type: object
  *       properties:
+ *         _id:
+ *           type: string
+ *           description: The ID of the order
  *         userId:
  *           type: string
  *           description: The ID of the user who placed the order
@@ -45,61 +48,62 @@ import {IProductDocument} from "./productModel";
  *           format: date-time
  *           description: The date and time when the order was created
  *       example:
+ *         _id: "60a9b8e5b6f9b7287418b16c"
  *         userId: "60a9b8e5b6f9b7287418b16c"
  *         userAddress:
  *           address: "123 Main Street, City"
  *           addressNumber: "456"
  *           zipCode: "12345"
  *           telephoneNumber: "+1234567890"
- *         items: [{ name: "Product 1", brand: "Brand A", price: 19.99, subCategoryId: "60a9b8e5b6f9b7287418b16c", description: "This is the description of Product 1", technicalSpecifications: { type: "single", diameter: "2.3 mm" }, quantity: 2, images: ["image1.jpg", "image2.jpg"] }]
- *         orderTotal: 50.99
+ *         items: [{ name: "Product 1", brand: "Brand A", price: 19.90, subCategoryId: "60a9b8e5b6f9b7287418b16c", description: "This is the description of Product 1", technicalSpecifications: { type: "single", diameter: "2.3 mm" }, quantity: 2, images: ["image1.jpg", "image2.jpg"] }]
  *         shippingCosts: 5.00
+ *         orderTotal: 44.80
  *         status: "pending"
  *         createdAt: "2024-01-10T12:34:56Z"
  */
 
 export interface IUserAddress {
-    address: string;
-    addressNumber: string;
-    zipCode: string;
-    telephoneNumber: string;
+  address: string;
+  addressNumber: string;
+  zipCode: string;
+  telephoneNumber: string;
 }
 
 export interface IOrderDocument extends mongoose.Document {
-    userId: mongoose.Schema.Types.ObjectId;
-    userAddress: IUserAddress;
-    items: IProductDocument[]; // Reusing the IProductDocument interface for order items
-    orderTotal: number;
-    shippingCosts: number;
-    status: string;
-    createdAt: Date;
+  userId: mongoose.Schema.Types.ObjectId;
+  userAddress: IUserAddress;
+  items: IProductDocument[]; // Reusing the IProductDocument interface for order items
+  orderTotal: number;
+  shippingCosts: number;
+  status: string;
+  createdAt: Date;
 }
 
 const orderSchema = new Schema<IOrderDocument>(
-    {
-        userId: {
-            type: Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        userAddress: {
-            address: {type: String, required: true},
-            addressNumber: {type: String, required: true},
-            zipCode: {type: String, required: true},
-            telephoneNumber: {type: String, required: true},
-        },
-        items: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Product",
-                required: true,
-            },
-        ],
-        shippingCosts: {type: Number, required: true},
-        orderTotal: {type: Number, required: true},
-        status: {type: String, default: "pending"},
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    {timestamps: true} // Automatically adds createdAt and updatedAt
+    userAddress: {
+      address: { type: String, required: true },
+      addressNumber: { type: String, required: true },
+      zipCode: { type: String, required: true },
+      telephoneNumber: { type: String, required: true },
+    },
+    items: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+    ],
+    shippingCosts: { type: Number, required: true },
+    orderTotal: { type: Number, required: true },
+    status: { type: String, default: "pending" },
+  },
+  { timestamps: true } // Automatically adds createdAt and updatedAt
 );
 
 const OrderModel: Model<IOrderDocument> = mongoose.model("Order", orderSchema);
