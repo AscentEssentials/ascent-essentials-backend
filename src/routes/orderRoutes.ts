1e6045c0382730715c33321d0ee2807c49d34157
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-09 12:03:55 +0100

    feat!: add route to get all orders of all users

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -35,4 +35,26 @@ const router = express.Router();
  */
 router.get("/orders", authenticateUser, OrderController.getAllOrdersOfActualUser);
 
+/**
+ * @swagger
+ * /admin/orders:
+ *   get:
+ *     tags: [Order]
+ *     summary: Get all orders of all users
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Returns all orders of all users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Order'
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
+router.get("/admin/orders", authenticateUser, isAdmin, OrderController.getAllOrders);
+
 export default router;
