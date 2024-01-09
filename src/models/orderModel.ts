675d286231f0a583b0217509c02e54ad929e270b
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-09 14:41:09 +0100

    docs: update swagger doc of orderModel

--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -15,25 +15,11 @@ import { IProductDocument } from "./productModel";
  *           type: string
  *           description: The ID of the user who placed the order
  *         userAddress:
- *           type: object
- *           description: The address details of the user
- *           properties:
- *             address:
- *               type: string
- *               description: The address of the user
- *             addressNumber:
- *               type: string
- *               description: The address number of the user
- *             zipCode:
- *               type: string
- *               description: The zip code of the user's address
- *             telephoneNumber:
- *               type: string
- *               description: The telephone number of the user
+ *           $ref: '#/components/schemas/UserAddress'
  *         items:
  *           type: array
  *           items:
- *             $ref: '#/components/schemas/Product'
+ *             $ref: '#/components/schemas/OrderItem'
  *         shippingCosts:
  *           type: number
  *           description: The cost of shipping for the order
@@ -55,11 +41,94 @@ import { IProductDocument } from "./productModel";
  *           addressNumber: "456"
  *           zipCode: "12345"
  *           telephoneNumber: "+1234567890"
- *         items: [{ name: "Product 1", brand: "Brand A", price: 19.90, subCategoryId: "60a9b8e5b6f9b7287418b16c", description: "This is the description of Product 1", technicalSpecifications: { type: "single", diameter: "2.3 mm" }, quantity: 2, images: ["image1.jpg", "image2.jpg"] }]
+ *         items:
+ *           - productId: "60a9b8e5b6f9b7287418b16c"
+ *             name: "Product 1"
+ *             brand: "Brand A"
+ *             price: 19.90
+ *             subCategoryId: "60a9b8e5b6f9b7287418b16c"
+ *             description: "This is the description of Product 1"
+ *             technicalSpecifications: { type: "single", diameter: "2.3 mm" }
+ *             quantity: 2
+ *             images: ["image1.jpg", "image2.jpg"]
  *         shippingCosts: 5.00
  *         orderTotal: 44.80
  *         status: "pending"
  *         createdAt: "2024-01-10T12:34:56Z"
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     UserAddress:
+ *       type: object
+ *       properties:
+ *         address:
+ *           type: string
+ *           description: The address of the user
+ *         addressNumber:
+ *           type: string
+ *           description: The address number of the user
+ *         zipCode:
+ *           type: string
+ *           description: The zip code of the user's address
+ *         telephoneNumber:
+ *           type: string
+ *           description: The telephone number of the user
+ *       example:
+ *         address: "123 Main Street, City"
+ *         addressNumber: "456"
+ *         zipCode: "12345"
+ *         telephoneNumber: "+1234567890"
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     OrderItem:
+ *       type: object
+ *       properties:
+ *         productId:
+ *           type: string
+ *           description: The ID of the product
+ *         name:
+ *           type: string
+ *           description: The name of the product
+ *         brand:
+ *           type: string
+ *           description: The brand of the product
+ *         price:
+ *           type: number
+ *           description: The price of the product
+ *         subCategoryId:
+ *           type: string
+ *           description: The ID of the sub-category of the product
+ *         description:
+ *           type: string
+ *           description: The description of the product
+ *         technicalSpecifications:
+ *           type: object
+ *           description: The technical specifications of the product
+ *         quantity:
+ *           type: number
+ *           description: The quantity of the product in the order
+ *         images:
+ *           type: array
+ *           items:
+ *             type: string
+ *           description: The images of the product
+ *       example:
+ *         productId: "60a9b8e5b6f9b7287418b16c"
+ *         name: "Product 1"
+ *         brand: "Brand A"
+ *         price: 19.90
+ *         subCategoryId: "60a9b8e5b6f9b7287418b16c"
+ *         description: "This is the description of Product 1"
+ *         technicalSpecifications: { type: "single", diameter: "2.3 mm" }
+ *         quantity: 2
+ *         images: ["image1.jpg", "image2.jpg"]
  */
 
 export interface IUserAddress {
