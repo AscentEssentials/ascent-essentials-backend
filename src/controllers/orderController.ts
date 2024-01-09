6f0508c85c24b17740d3d7d4d192bede905c3eb6
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-09 11:46:42 +0100

    refactor: define a function to map an IOrderDocument to a response object respecting the Order schema

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -19,32 +19,44 @@ export class OrderController {
       const user = req.user as IUserDocument;
       const userId = user._id;
 
-      const orders: IOrderDocument[] = await OrderModel.find({ userId }).sort({ createdAt: -1 });
+      const orders: IOrderDocument[] = await OrderModel.find({ userId }).sort({
+        createdAt: -1,
+      });
       // Response to respect the array of Order schema.
-      const response = orders.map((order) => ({
-        _id: order._id,
-        userId: order.userId,
-        userAddress: order.userAddress,
-        items: order.items.map((item) => ({
-          name: item.name,
-          brand: item.brand,
-          price: item.price,
-          subCategoryId: item.subCategoryId,
-          description: item.description,
-          technicalSpecifications: item.technicalSpecifications,
-          quantity: item.quantity,
-          images: item.images,
-        })),
-        shippingCosts: order.shippingCosts,
-        orderTotal: order.orderTotal,
-        status: order.status,
-        createdAt: order.createdAt,
-      }));
+      const response = orders.map((order) =>
+        OrderController.mapOrderToResponse(order)
+      );
+
       res.status(200).json(response);
     } catch (error) {
       console.error("[OrderController] Error fetching orders:", error);
       res.status(500).send("Internal Server Error");
     }
+  }
+
+  /**
+   * Private function to map an IOrderDocument to a response object respecting the Order schema.
+   */
+  private static mapOrderToResponse(order: IOrderDocument) {
+    return {
+      _id: order._id,
+      userId: order.userId,
+      userAddress: order.userAddress,
+      items: order.items.map((item) => ({
+        name: item.name,
+        brand: item.brand,
+        price: item.price,
+        subCategoryId: item.subCategoryId,
+        description: item.description,
+        technicalSpecifications: item.technicalSpecifications,
+        quantity: item.quantity,
+        images: item.images,
+      })),
+      shippingCosts: order.shippingCosts,
+      orderTotal: order.orderTotal,
+      status: order.status,
+      createdAt: order.createdAt,
+    };
   }
 }
 
