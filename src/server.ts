3a5399b6c86480a115d3b9b7d3a52c00b3ec7e96
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-10 21:14:29 +0100

    feat: console log when socket.io connection event

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
-import app from "./app";
+import server, { io } from "./app";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+// Socket.IO connection event
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
