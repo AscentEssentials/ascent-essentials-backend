f5f931fb896b3fe46e04365c817e8297c2f86860
Eddie Barzi <eddie.barzi@studio.unibo.it>
2024-01-10 21:14:29 +0100

    feat: console log when socket.io connection event

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express, { Express } from "express";
 import routes from "./routes/routes";
 import mongoose from "mongoose";
 import cors from "cors";
+import http from "http";
+import { Server as SocketIOServer } from "socket.io";
 
 const app: Express = express();
+const server = http.createServer(app); // Create an HTTP server, needed for Socket.IO
 
 // Middleware to parse JSON in the request body
 app.use(express.json());
@@ -25,4 +28,7 @@ mongoose
     console.log("[mongoose]: Error connecting to the database", err);
   });
 
-export default app;
+// Create a Socket.IO server and attach it to the HTTP server
+export const io = new SocketIOServer(server);
+
+export default server;
